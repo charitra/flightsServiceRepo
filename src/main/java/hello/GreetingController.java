073595910ddfdbf6d5f360a24dd0305f72a43b68
charitra <charitrakocheri@gmail.com>
2016-10-18 11:19:50 +0530

    addingdata

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,6 +3,9 @@ package hello;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.json.simple.JSONArray;
@@ -24,79 +27,41 @@ public class GreetingController {
     @RequestMapping(method = RequestMethod.POST, value = "/SearchFlight")
     public Response SearchFlight() {
     	String readJson = readJson();
-        return new Response(template1+readJson, template1+readJson, new ArrayList<String>(), new ArrayList<String>(), 
+    	Map<String, String> flightData = new HashMap<>();
+    	flightData.put("flightNumber", "AI123");
+    	flightData.put("deptTime", "0800");
+    	flightData.put("arrivalTime", "1200");
+    	flightData.put("stops", "0");
+    	
+    	Map<String, String> flightData1 = new HashMap<>();
+    	flightData1.put("flightNumber", "AI321");
+    	flightData1.put("deptTime", "1200");
+    	flightData1.put("arrivalTime", "1500");
+    	flightData1.put("stops", "0");
+    	
+    	List<Map<String, String>> data = new ArrayList<>();
+    	data.add(flightData);
+    	data.add(flightData1);
+        return new Response(template1+readJson, template1+readJson, data, new ArrayList<String>(), 
         		"MakeMyTrip");
     }
     
-    @RequestMapping(method = RequestMethod.POST, value = "/bookFlight")
+    /*@RequestMapping(method = RequestMethod.POST, value = "/bookFlight")
     public Response bookFlight() {
-        return new Response(template2, template2, new ArrayList<String>(), new ArrayList<String>(), 
+        return new Response(template2, template2, data, new ArrayList<String>(), 
         		"MakeMyTrip");
-    }
+    }*/
     
-    @RequestMapping("/welcome")
+    /*@RequestMapping("/welcome")
     public Response welcome() {
         return new Response("Welcome to Flights booking", "Welcome to Flights booking"
         		, new ArrayList<String>(), new ArrayList<String>(), "MakeMyTrip");
     }
-    
+    */
     public String readJson() {
     	String inline = "inline";
     	String jsonarr_2 = "action";
-    	try
-		{
-			URL url = new URL("http://maps.googleapis.com/maps/api/geocode/json?address=chicago&sensor=false&#8221");
-			//Parse URL into HttpURLConnection in order to open the connection in order to get the JSON data
-			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-			//Set the request to GET or POST as per the requirements
-			conn.setRequestMethod("POST");
-			//Use the connect method to create the connection bridge
-			conn.connect();
-			//Get the response status of the Rest API
-			int responsecode = conn.getResponseCode();
-			System.out.println("Response code is: " +responsecode);
-			
-			//Iterating condition to if response code is not 200 then throw a runtime exception
-			//else continue the actual process of getting the JSON data
-			if(responsecode != 200)
-				throw new RuntimeException("HttpResponseCode: " +responsecode);
-			else
-			{
-				//Scanner functionality will read the JSON data from the stream
-				Scanner sc = new Scanner(url.openStream());
-				while(sc.hasNext())
-				{
-					inline+=sc.nextLine();
-				}
-				System.out.println("\nJSON Response in String format"); 
-				System.out.println(inline);
-				//Close the stream when reading the data has been finished
-				sc.close();
-			}
-			
-			//JSONParser reads the data from string object and break each data into key value pairs
-			JSONParser parse = new JSONParser();
-			//Type caste the parsed json data in json object
-			JSONObject jobj = (JSONObject)parse.parse(inline);
-			//Store the JSON object in JSON array as objects (For level 1 array element i.e Results)
-			JSONArray jsonarr_1 = (JSONArray) jobj.get("result");
-			//Get data for Results array
-			for(int i=0;i<jsonarr_1.size();i++)
-			{
-				//Store the JSON objects in an array
-				//Get the index of the JSON object and print the values as per the index
-				JSONObject jsonobj_1 = (JSONObject)jsonarr_1.get(i);
-				//Store the JSON object in JSON array as objects (For level 2 array element i.e Address Components)
-				jsonarr_2 = (String) jsonobj_1.get("action");
-				
-			}
-			//Disconnect the HttpURLConnection stream
-			conn.disconnect();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+    	
     	return inline;
     }
 }
--- src/main/java/hello/Response.java
+++ src/main/java/hello/Response.java
@@ -1,16 +1,17 @@
 package hello;
 
 import java.util.List;
+import java.util.Map;
 
 public class Response {
 
 	private String speech;
 	private String displayText;
-	private List<String> data;
+	private List<Map<String, String>> data;
 	private List<String> contextOut;
 	private String source;
 	
-	public Response(String speech, String displayText, List<String> data,
+	public Response(String speech, String displayText, List<Map<String, String>> data,
 			List<String> contextOut, String source) {
 		this.speech = speech;
 		this.displayText = displayText;
@@ -30,10 +31,10 @@ public class Response {
 	public void setDisplayText(String displayText) {
 		this.displayText = displayText;
 	}
-	public List<String> getData() {
+	public List<Map<String, String>> getData() {
 		return data;
 	}
-	public void setData(List<String> data) {
+	public void setData(List<Map<String, String>> data) {
 		this.data = data;
 	}
 	public List<String> getContextOut() {
